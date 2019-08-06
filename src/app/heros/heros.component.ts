382610303b7584dd2c4087785b63f6288eaae060
Machine100 <joesmileyyy@yahoo.com>
2019-08-06 12:34:42 -0400

    .

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,17 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
+import {HEROS} from '../mock-heros';
 
 @Component({
   selector: 'app-heros',
   templateUrl: './heros.component.html',
   styleUrls: ['./heros.component.css']
 })
+
 export class HerosComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  hero: Hero = {id: 1, name: 'Windstorm'};
+  heros: HEROS;
 
   constructor() { }
 
