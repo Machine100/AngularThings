54c0d33889e4205bc95c4c4f7193c07b8c032edf
Machine100 <joesmileyyy@yahoo.com>
2019-09-05 10:34:09 -0400

    .

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router'
 import { LoginComponent } from '../auth/login/login.component'
 import { LogoutComponent } from '../auth/logout/logout.component'
 import { RegisterComponent } from '../auth/register/register.component'
+import { ReaddatabaseComponent } from '../components/readdatabase/readdatabase.component';
 
 
 const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'logout', component: LogoutComponent},
-    { path: 'register', component: RegisterComponent}
+    { path: 'register', component: RegisterComponent},
+    { path: 'readdatabase', component: ReaddatabaseComponent, outlet: 'outlet2'}
 ]
 
 @NgModule({
